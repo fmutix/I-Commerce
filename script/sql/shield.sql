ddf2cb438fb61118ec8c34612727731659d11100
fmutix <fmutix@gmail.com>
2015-03-28 20:52:59 +0000

    database shields

---
+++ script/sql/shield.sql
@@ -0,0 +1,38 @@
+/* Boucliers */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Bouclier Romain', 'Bouclier', 'Bouclier', 200, 2, 4, 'res/img/objets/obj_48.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Bouclier Rond', 'Bouclier', 'Bouclier', 500, 4, 5, 'res/img/objets/obj_53.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Evasion,Path)
+	VALUES ('Bouclier Teuton', 'Bouclier', 'Bouclier', 1000, 2, 6, 7, 'res/img/objets/obj_54.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Bouclier Gel', 'Bouclier', 'Bouclier', 1500, 6, 9, 'res/img/objets/obj_55.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Bouclier Flamme', 'Bouclier', 'Bouclier', 1500, 6, 9, 'res/img/objets/obj_56.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Evasion,Path)
+	VALUES ('Bouclier Aegis', 'Bouclier', 'Bouclier', 3000, 5, 5, 10, 'res/img/objets/obj_57.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Evasion,Path)
+	VALUES ('Bouclier Genji', 'Bouclier', 'Bouclier', 12000, 10, ,7, 10, 'res/img/objets/obj_58.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Evasion,Path)
+	VALUES ('Bouclier Sacré', 'Bouclier', 'Bouclier', 6000, 5, ,5, 10, 'res/img/objets/obj_59.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Bouclier Shijin', 'Bouclier', 'Bouclier', 4000, 10, 7, 'res/img/objets/obj_60.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Evasion,Path)
+	VALUES ('Chocobouclier', 'Bouclier', 'Bouclier', 4200, 10, 'res/img/objets/obj_61.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Resistance,Evasion,Path)
+	VALUES ('Séraphica', 'Bouclier', 'Bouclier', 7000, 5, 15, 'res/img/objets/obj_62.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Evasion,Path)
+	VALUES ('Bouclier Songe', 'Bouclier', 'Bouclier', 8200, 5, ,10, 10, 'res/img/objets/obj_63.gif');
