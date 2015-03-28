a331a34cefd800da5051d75ad6f3d9a9e2395ff5
fmutix <fmutix@gmail.com>
2015-03-28 11:10:08 +0000

    insert casques

--- script/sql/table.sql
+++ script/sql/table.sql
@@ -17,7 +17,36 @@ CREATE TABLE Item(
 	Move NUMERIC(1,0),
 	Jump NUMERIC(1,0),
 	Evasion NUMERIC(1,0),
+	Path VARCHAR(255),
 	PRIMARY KEY(Name)
 );
 
-
+/* Casques */
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Romain', 'Tête', 'Casque', 250, 4, 2, 'res/img/objets/obj_1.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque fer', 'Tête', 'Casque', 600, 5, 3, 'res/img/objets/obj_2.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Teunon', 'Tête', 'Casque', 1250, 7, 3, 'res/img/objets/obj_30.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Noble', 'Tête', 'Casque', 2000, 9, 4, 'res/img/objets/obj_31.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Diamant', 'Tête', 'Casque', 5000, 11, 5, 'res/img/objets/obj_32.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Genji', 'Tête', 'Casque', 10000, 15, 6, 'res/img/objets/obj_33.gif');
+	
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Parade', 'Tête', 'Casque', 8000, 13, 4, 'res/img/objets/obj_34.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Hanya', 'Tête', 'Casque', 10000, 12, 8, 'res/img/objets/obj_35.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Casque Vangaa', 'Tête', 'Casque', 12000, 16, 6, 'res/img/objets/obj_36.gif');
