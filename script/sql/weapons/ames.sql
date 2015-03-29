e1c7a1c89802a1156cb68a0c39f8d81fe491138b
fmutix <fmutix@gmail.com>
2015-03-29 15:01:34 +0100

    ames.sql

---
+++ script/sql/weapons/ames.sql
@@ -0,0 +1,38 @@
+/* Ames */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Evasion,Price,Path)
+	VALUES ('Ame gobelin', 'Arme', 'Ame', 32, 2, 1, 700, 'res/img/objets/obj_352.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Magic,Price,Path)
+	VALUES ('Ame flan', 'Arme', 'Ame', 34, 10, 2, 1000, 'res/img/objets/obj_353.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Ame bombo', 'Arme', 'Ame', 36, 2, 1200, 'res/img/objets/obj_354.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Magic,Price,Path)
+	VALUES ('Ame dragon', 'Arme', 'Ame', 43, 5, 2, 2000, 'res/img/objets/obj_355.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Ame ramya', 'Arme', 'Ame', 41, 2, 1500, 'res/img/objets/obj_356.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Magic,Price,Path)
+	VALUES ('Ame insecte', 'Arme', 'Ame', 39, 2, 2, 1300, 'res/img/objets/obj_357.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Evasion,Price,Path)
+	VALUES ('Ame panthère', 'Arme', 'Ame', 39, 2, 2, 2000, 'res/img/objets/obj_358.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Ame xylomid', 'Arme', 'Ame', 47, 2, 2, 3000, 'res/img/objets/obj_359.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Ame oeil', 'Arme', 'Ame', 45, 2, 1400, 'res/img/objets/obj_360.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Ame mythril', 'Arme', 'Ame', 32, 1, 2000, 'res/img/objets/obj_361.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Ame effroi', 'Arme', 'Ame', 49, 2, 22	00, 'res/img/objets/obj_362.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Ame rukavi', 'Arme', 'Ame', 67, 2, 8100, 'res/img/objets/obj_363.gif');
--- script/sql/weapons/arme.sql
+++ script/sql/weapons/arme.sql
@@ -1,38 +1,38 @@
 /* Armes */
 
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme Aiot', 'Arme', 'Arme', 27, 1000, 'res/img/objets/obj_364.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Canon d\'argent', 'Arme', 'Arme', 31, 1500, 'res/img/objets/obj_365.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme émeute', 'Arme', 'Arme', 31, 2500, 'res/img/objets/obj_366.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Fusil chaos', 'Arme', 'Arme', 33, 4000, 'res/img/objets/obj_367.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme perdue', 'Arme', 'Arme', 31, 1200, 'res/img/objets/obj_368.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Ransei', 'Arme', 'Arme', 33, 2000, 'res/img/objets/obj_369.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme Giot', 'Arme', 'Arme', 37, 3000, 'res/img/objets/obj_370.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Longbarrel', 'Arme', 'Arme', 39, 6000, 'res/img/objets/obj_371.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Outsider', 'Arme', 'Arme', 41, 8000, 'res/img/objets/obj_372.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme mythril', 'Arme', 'Arme', 32, 2000, 'res/img/objets/obj_373.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Bindsnipe', 'Arme', 'Arme', 47, 10000, 'res/img/objets/obj_374.gif');
 INSERT INTO 
-	APP.ITEM (Name,Type,Category,AttackPrice,Path)
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
 	VALUES ('Arme d\'appel', 'Arme', 'Arme', 59, 15000, 'res/img/objets/obj_375.gif');
