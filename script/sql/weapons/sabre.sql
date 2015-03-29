ce854c06d6d9d19bb004ac06f4ab4537c2a7b35a
nepta <nepta23@gmail.com>
2015-03-29 15:46:00 +0200

    sabres

---
+++ script/sql/weapons/sabre.sql
@@ -0,0 +1,26 @@
+/* Sabre */
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Path)
+	VALUES ('Sabre bleu', 'Arme', 'Sabre', 500, 25, 2, 'res/img/objets/obj_181.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Evasion,Path)
+	VALUES ('Shamshir', 'Arme', 'Sabre', 1100, 31,2, 'res/img/objets/obj_182.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Evasion,Path)
+	VALUES ('Aqua sabre', 'Arme', 'Sabre', 1850, 36, 2, 'res/img/objets/obj_183.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Path)
+	VALUES ('Cimeterre', 'Arme', 'Sabre', 1000, 42, 5, 'res/img/objets/obj_184.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Evasion,Path)
+	VALUES ('Manganèse', 'Arme', 'Sabre', 500, 47, 10, 3, 'res/img/objets/obj_185.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Jump,Path)
+	VALUES ('Sabre mythril', 'Arme', 'Sabre', 2000, 32, 1, 'res/img/objets/obj_186.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Speed,Path)
+	VALUES ('Tulwar', 'Arme', 'Sabre', 3000, 55, 10, 10, 2, 'res/img/objets/obj_187.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Resistance,Evasion,Path)
+	VALUES ('Sabre âme', 'Arme', 'Sabre', 500, 39, 10, 5, 'res/img/objets/obj_188.gif');
+
