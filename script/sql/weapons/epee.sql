a38d9d5e130f5175bcd9fcfac6fb7210a3ef2147
nepta <nepta23@gmail.com>
2015-03-29 14:57:56 +0200

    epee

---
+++ script/sql/weapons/epee.sql
@@ -0,0 +1,41 @@
+/* Epée */
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Epée courte', 'Arme', 'Epée', 150, 25, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Evasion,Path)
+	VALUES ('Epée argent', 'Arme', 'Epée', 450, 30, 2, 2, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Epée griffe', 'Arme', 'Epée', 800, 35, 5, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Epée filou', 'Arme', 'Epée', 1500, 39, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Path)
+	VALUES ('Epée rafale', 'Arme', 'Epée', 500, 36, 1, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Epée vermeille', 'Arme', 'Epée', 1000, 18, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Resistance,Path)
+	VALUES ('Raviveur', 'Arme', 'Epée', 2000, 40, 5, 5, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Evasion,Path)
+	VALUES ('Vitanova', 'Arme', 'Epée', 4000, 38, 2, 2, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Jump,Path)
+	VALUES ('Epée mythril', 'Arme', 'Epée', 2000, 33, 1, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Path)
+	VALUES ('Epée victoire', 'Arme', 'Epée', 500, 33, 10, 10, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Evasion,Path)
+	VALUES ('Epée bulbe', 'Arme', 'Epée', 500, 29, 5, 5, 10, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Chijiraden', 'Arme', 'Epée', 500, 65, 'res/img/objets/obj_16.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack, Magic,Path)
+	VALUES ('Epée de glace', 'Arme', 'Epée', 500, 41, 5, 'res/img/objets/obj_16.gif');
+
