c594cc06c5be97261acf3cb15a8a432f36d55411
fmutix <fmutix@gmail.com>
2015-03-29 14:49:51 +0100

    arcs longs

---
+++ script/sql/weapons/arcs_longs.sql
@@ -0,0 +1,47 @@
+/* Arcs longs */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Arc zéphyr', 'Arme', 'Arc long', 25, 2, 1500, 'res/img/objets/obj_297.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc forestier', 'Arme', 'Arc long', 23, 1000, 'res/img/objets/obj_298.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc composite', 'Arme', 'Arc long', 29, 2500, 'res/img/objets/obj_299.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc double', 'Arme', 'Arc long', 31, 4000, 'res/img/objets/obj_300.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc chasseur', 'Arme', 'Arc long', 33, 5000, 'res/img/objets/obj_301.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Arc Fay', 'Arme', 'Arc long', 31, 2, 2000, 'res/img/objets/obj_302.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc Hadès', 'Arme', 'Arc long', 33, 5000, 'res/img/objets/obj_303.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Price,Path)
+	VALUES ('Arc spécial', 'Arme', 'Arc long', 37, 5, 7000, 'res/img/objets/obj_304.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Arc suprême', 'Arme', 'Arc long', 41, 2, 10000, 'res/img/objets/obj_305.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Arc promesse', 'Arme', 'Arc long', 61, 2, 14000, 'res/img/objets/obj_306.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Septième ciel', 'Arme', 'Arc long', 15, 5, 14500, 'res/img/objets/obj_307.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Mythril shot', 'Arme', 'Arc long', 32, 1, 2000, 'res/img/objets/obj_308.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc marduk', 'Arme', 'Arc long', 39, 5000, 'res/img/objets/obj_309.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc gasutra', 'Arme', 'Arc long', 51, 11000, 'res/img/objets/obj_310.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arbalète', 'Arme', 'Arc long', 42, 7500, 'res/img/objets/obj_301.gif');
