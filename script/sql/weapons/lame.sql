9945b17f93cf16d3bdd0f3dd0ac5ccdc41407436
nepta <nepta23@gmail.com>
2015-03-29 15:39:01 +0200

    lame

---
+++ script/sql/weapons/lame.sql
@@ -0,0 +1,56 @@
+/* lame */
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Lame asalto', 'Arme', 'Lame', 750, 28, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Evasion,Path)
+	VALUES ('Lame fantôme', 'Arme', 'Lame', 1250, 28, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Path)
+	VALUES ('Lame soleil', 'Arme', 'Lame', 1500, 37, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Lame atmos', 'Arme', 'Lame', 2000, 36, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Langue de feu', 'Arme', 'Lame', 2500, 38, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Lame de vent', 'Arme', 'Lame', 3500, 40, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Glaive neige', 'Arme', 'Lame', 4000, 42, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Path)
+	VALUES ('Lame kwigon', 'Arme', 'Lame', 5000, 40, 3, 3, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,MagicPath)
+	VALUES ('Lame ogun', 'Arme', 'Lame', 7500, 42, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Path)
+	VALUES ('Lame perle', 'Arme', 'Lame', 2000, 46, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Evasion,Path)
+	VALUES ('Lame palaiva', 'Arme', 'Lame', 11000, 33, 10, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Speed,Path)
+	VALUES ('Lame vénus', 'Arme', 'Lame', 3000, 45, 2, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Resistance,Path)
+	VALUES ('Lame matéria', 'Arme', 'Lame', 500, 17, 15, 10, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Jump,Path)
+	VALUES ('Lame mythril', 'Arme', 'Lame', 2000, 32,1, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Lame ébon', 'Arme', 'Lame', 500, 84, 5, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Lame adaman', 'Arme', 'Lame', 500, 65, 15, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Speed,Path)
+	VALUES ('Ayvuir rouge', 'Arme', 'Lame', 500, 62, 10, 2, 'res/img/objets/obj_3.gif');
+INSERT INTO
+	APP.ITEM (Name,Type,Category,Price,Attack,Resistance,Evasion,Path)
+	VALUES ('Ayvuir bleu', 'Arme', 'Lame', 500, 51, 10, 2, 'res/img/objets/obj_3.gif');
+
