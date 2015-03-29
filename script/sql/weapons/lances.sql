67c97982872add2956c2f696523187ff86bcf508
fmutix <fmutix@gmail.com>
2015-03-29 14:42:48 +0100

    lances.sql

---
+++ script/sql/weapons/lances.sql
@@ -0,0 +1,38 @@
+/* Lances */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Javelot', 'Arme', 'Lance', 31, 1500, 'res/img/objets/obj_312.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Lane lave', 'Arme', 'Lance', 33, 2500, 'res/img/objets/obj_313.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Gae Bolg', 'Arme', 'Lance', 39, 7000, 'res/img/objets/obj_314.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Lance glace', 'Arme', 'Lance', 35, 4000, 'res/img/objets/obj_315.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Partisan', 'Arme', 'Lance', 42, 1, 5000, 'res/img/objets/obj_316.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Lace de Caïn', 'Arme', 'Lance', 47, 1, 3000, 'res/img/objets/obj_317.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Jump,Price,Path)
+	VALUES ('Trident', 'Arme', 'Lance', 50, 2, 1, 6000, 'res/img/objets/obj_318.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Poil de dragon', 'Arme', 'Lance', 45, 1, 9000, 'res/img/objets/obj_319.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Lance mythril', 'Arme', 'Lance', 32, 2, 2000, 'res/img/objets/obj_320.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Lance Odin', 'Arme', 'Lance', 55, 12000, 'res/img/objets/obj_321.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Price,Path)
+	VALUES ('Lance bestiale', 'Arme', 'Lance', 51, 10, 14000, 'res/img/objets/obj_322.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Magic,Resistance,Price,Path)
+	VALUES ('Pique Vangaa', 'Arme', 'Lance', 53, 5, 2, 5, 18000, 'res/img/objets/obj_323.gif');
