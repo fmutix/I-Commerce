667c1b035d141c0e95a3eef24bcfc62c3a3224a4
fmutix <fmutix@gmail.com>
2015-03-29 14:24:18 +0100

    instruments.sql

---
+++ script/sql/weapons/instruments.sql
@@ -0,0 +1,44 @@
+/* Instruments */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Cloche démon', 'Arme', 'Instrument', 22, 200, 'res/img/objets/obj_324.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Resistance,Price,Path)
+	VALUES ('Cloche verre', 'Arme', 'Instrument', 25, 1, 500, 'res/img/objets/obj_325.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Clairon', 'Arme', 'Instrument', 25, 1, 850, 'res/img/objets/obj_326.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Price,Path)
+	VALUES ('Conque', 'Arme', 'Instrument', 31, 2, 1500, 'res/img/objets/obj_327.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Price,Path)
+	VALUES ('Cloche terre', 'Arme', 'Instrument', 31, 3, 2500, 'res/img/objets/obj_328.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Resistance,Price,Path)
+	VALUES ('Black Quena', 'Arme', 'Instrument',33, 2, 3500, 'res/img/objets/obj_329.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Satyr Flute', 'Arme', 'Instrument', 35, 1, 5000, 'res/img/objets/obj_330.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Price,Path)
+	VALUES ('Harpe de fée', 'Arme', 'Instrument', 29, 2, 1000, 'res/img/objets/obj_331.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Resistance,Price,Path)
+	VALUES ('Aona Flute', 'Arme', 'Instrument', 32, 2, 4000, 'res/img/objets/obj_332.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Carillon soin', 'Arme', 'Instrument', 39, 1, 8000, 'res/img/objets/obj_333.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Harpe sang', 'Arme', 'Instrument', 22, 1, 10000, 'res/img/objets/obj_334.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Cloche mythril', 'Arme', 'Instrument', 32, 1, 2000, 'res/img/objets/obj_335.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Sombre violon', 'Arme', 'Instrument', 45, 9000, 'res/img/objets/obj_336.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Castagnettes', 'Arme', 'Instrument', 47, 11500, 'res/img/objets/obj_337.gif');
