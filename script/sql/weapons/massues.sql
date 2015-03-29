4cc0c8cb3634bad93666b7fbe678194eacc87045
fmutix <fmutix@gmail.com>
2015-03-29 15:02:01 +0100

    massues

---
+++ script/sql/weapons/massues.sql
@@ -0,0 +1,41 @@
+/* Massues */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Masse combat', 'Arme', 'Massue', 31, 1000, 'res/img/objets/obj_284.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Masse energy', 'Arme', 'Massue', 29, 2, 5, 1750, 'res/img/objets/obj_285.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Masse druide', 'Arme', 'Massue', 33, 3, 6, 3000, 'res/img/objets/obj_286.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Masse de sage', 'Arme', 'Massue', 31, 8, 8, 4500, 'res/img/objets/obj_287.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Morning Star', 'Arme', 'Massue', 33, 2, 5, 1300, 'res/img/objets/obj_288.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Mandragore', 'Arme', 'Massue', 37, 2, 5, 2000, 'res/img/objets/obj_289.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Bâton de vie', 'Arme', 'Massue', 35, 2, 10, 6000, 'res/img/objets/obj_290.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Masse lotus', 'Arme', 'Massue', 37, 2, 5, 8000, 'res/img/objets/obj_291.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Scorpion', 'Arme', 'Massue', 38, 2, 5, 10000, 'res/img/objets/obj_292.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Masse Zeus', 'Arme', 'Massue', 45, 5, 5, 12000, 'res/img/objets/obj_293.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Jump,Price,Path)
+	VALUES ('Masse mythril', 'Arme', 'Massue', 32, 2, 5, 1, 8000, 'res/img/objets/obj_294.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Cactus sting', 'Arme', 'Massue', 62, 2, 5, 15000, 'res/img/objets/obj_295.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Price,Path)
+	VALUES ('Vesper', 'Arme', 'Massue', 39, 2, 5, 13400, 'res/img/objets/obj_296.gif');
