8f5af9c46f1be40b080f02c81e8b8dc900e66a0d
fmutix <fmutix@gmail.com>
2015-03-29 14:55:27 +0100

    arcs

---
+++ script/sql/weapons/arcs.sql
@@ -0,0 +1,38 @@
+/* Arcs */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc long', 'Arme', 'Arc', 19, 150, 'res/img/objets/obj_5.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc charbon', 'Arme', 'Arc', 21, 350, 'res/img/objets/obj_6.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc épine', 'Arme', 'Arc', 25, 750, 'res/img/objets/obj_7.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc écaille', 'Arme', 'Arc', 29, 2000, 'res/img/objets/obj_8.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc argent', 'Arme', 'Arc', 23, 1250, 'res/img/objets/obj_9.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc Artémis', 'Arme', 'Arc', 27, 1300, 'res/img/objets/obj_10.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc Yoichi', 'Arme', 'Arc', 33, 1500, 'res/img/objets/obj_11.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Arc mille', 'Arme', 'Arc', 35, 5, 3000, 'res/img/objets/obj_12.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Arc Persée', 'Arme', 'Arc', 42, 2, 5000, 'res/img/objets/obj_13.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Price,Path)
+	VALUES ('Arc mythril', 'Arme', 'Arc', 132, 1, 2000, 'res/img/objets/obj_50.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Arc lune', 'Arme', 'Arc', 45, 7000, 'res/img/objets/obj_51.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Price,Path)
+	VALUES ('Malarc', 'Arme', 'Arc', 55, 9500, 'res/img/objets/obj_52.gif');
