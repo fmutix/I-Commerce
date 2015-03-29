ceb19b079774c85f100522cf2b8d6c2bc3f7d645
fmutix <fmutix@gmail.com>
2015-03-29 15:11:59 +0100

    griffes.sql

---
+++ script/sql/weapons/griffes.sql
@@ -0,0 +1,44 @@
+/* Griffes */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Poings d\'acier', 'Arme', 'Griffes', 29, 1, 250, 'res/img/objets/obj_338.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Rising Sun', 'Arme', 'Griffes', 31, 1, 500, 'res/img/objets/obj_339.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Poings venin', 'Arme', 'Griffes', 35, 1, 1000, 'res/img/objets/obj_340.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Griffes Morphée', 'Arme', 'Griffes', 39, 1, 1600, 'res/img/objets/obj_341.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Poings Kaiser', 'Arme', 'Griffes', 42, 1, 2850, 'res/img/objets/obj_342.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Speed,Evasion,Price,Path)
+	VALUES ('Griffes de chat', 'Arme', 'Griffes', 35, 2, 1, 1500, 'res/img/objets/obj_343.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Defense,Evasion,Price,Path)
+	VALUES ('Survivor', 'Arme', 'Griffes', 37, 2, 2, 2000, 'res/img/objets/obj_344.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Crocs blancs', 'Arme', 'Griffes', 39, 1, 4000, 'res/img/objets/obj_345.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Magic,Resistance,Evasion,Price,Path)
+	VALUES ('God Hand', 'Arme', 'Griffes', 39, 3, 1, 5, 6000, 'res/img/objets/obj_346.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Crocs tigre', 'Arme', 'Griffes', 41, 2, 8000, 'res/img/objets/obj_347.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Griffes trépass', 'Arme', 'Griffes', 43, 1, 10000, 'res/img/objets/obj_348.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Jump,Evasion,Price,Path)
+	VALUES ('Griffes mythril', 'Arme', 'Griffes', 32, 1, 1, 2000, 'res/img/objets/obj_349.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Greaseburst', 'Arme', 'Griffes', 59, 1, 13500, 'res/img/objets/obj_350.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Attack,Evasion,Price,Path)
+	VALUES ('Magic Hands', 'Arme', 'Griffes', 52, 1, 12000, 'res/img/objets/obj_351.gif');
