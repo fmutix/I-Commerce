624470e5166d69968c306e611be1aef863d239af
nepta <nepta23@gmail.com>
2015-03-29 14:21:06 +0200

    Gantelets

--- script/sql/miscellaneous.sql
+++ script/sql/miscellaneous.sql
@@ -1,4 +1,5 @@
 /* Bottes */
+
 INSERT INTO 
 	APP.ITEM (Name,Type,Category,Defense,Price,Path)
 	VALUES ('Bottes Combat', 'Divers', 'Bottes', 7, 500, 'res/img/objets/obj_115.gif');
@@ -29,5 +30,27 @@ INSERT INTO
 INSERT INTO 
 	APP.ITEM (Name,Type,Category,Defense,Move,Price,Path)
 	VALUES ('Chaussons Ninja', 'Divers', 'Bottes', 3, 2, 500, 'res/img/objets/obj_124.gif');
+
+
+/* Gantelets */
+
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Gantelets', 'Divers', 'Gantelets', 1000, 5, 5, 'res/img/objets/obj_125.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Gants voleur', 'Divers', 'Gantelets', 1500, 3, 3, 'res/img/objets/obj_126.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
+	VALUES ('Bracers', 'Divers', 'Gantelets', 500, 5, 12, 'res/img/objets/obj_127.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Magic,Resistance,Path)
+	VALUES ('Gants genji', 'Divers', 'Gantelets', 500, 5, 10, 2, 5, 'res/img/objets/obj_128.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Defense,Resistance,Path)
+	VALUES ('Mitaines feu', 'Divers', 'Gantelets', 500, 8, 10, 'res/img/objets/obj_129.gif');
+INSERT INTO 
+	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Evasion,Path)
+	VALUES ('Gants ivoire', 'Divers', 'Gantelets', 500, 3, 6, 8, 5, 'res/img/objets/obj_130.gif');
 
 
