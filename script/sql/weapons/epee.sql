65e888171b31c233c70d6133a231d7cc846cc45e
fmutix <fmutix@gmail.com>
2015-03-30 15:31:13 +0200

    Epée -> Epee, category Arme -> Pistolet

--- script/sql/weapons/epee.sql
+++ script/sql/weapons/epee.sql
@@ -1,41 +1,41 @@
-/* Epée */
+/* Epee */
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Path)
-	VALUES ('Epée courte', 'Arme', 'Epée', 150, 25, 'res/img/objets/obj_3.gif');
+	VALUES ('Epee courte', 'Arme', 'Epee', 150, 25, 'res/img/objets/obj_3.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Evasion,Path)
-	VALUES ('Epée argent', 'Arme', 'Epée', 450, 30, 2, 2, 'res/img/objets/obj_151.gif');
+	VALUES ('Epee argent', 'Arme', 'Epee', 450, 30, 2, 2, 'res/img/objets/obj_151.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Path)
-	VALUES ('Epée griffe', 'Arme', 'Epée', 800, 35, 5, 'res/img/objets/obj_152.gif');
+	VALUES ('Epee griffe', 'Arme', 'Epee', 800, 35, 5, 'res/img/objets/obj_152.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Path)
-	VALUES ('Epée filou', 'Arme', 'Epée', 1500, 39, 'res/img/objets/obj_153.gif');
+	VALUES ('Epee filou', 'Arme', 'Epee', 1500, 39, 'res/img/objets/obj_153.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Speed,Path)
-	VALUES ('Epée rafale', 'Arme', 'Epée', 500, 36, 1, 'res/img/objets/obj_154.gif');
+	VALUES ('Epee rafale', 'Arme', 'Epee', 500, 36, 1, 'res/img/objets/obj_154.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Path)
-	VALUES ('Epée vermeille', 'Arme', 'Epée', 1000, 18, 'res/img/objets/obj_155.gif');
+	VALUES ('Epee vermeille', 'Arme', 'Epee', 1000, 18, 'res/img/objets/obj_155.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Resistance,Path)
-	VALUES ('Raviveur', 'Arme', 'Epée', 2000, 40, 5, 5, 'res/img/objets/obj_156.gif');
+	VALUES ('Raviveur', 'Arme', 'Epee', 2000, 40, 5, 5, 'res/img/objets/obj_156.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Magic,Evasion,Path)
-	VALUES ('Vitanova', 'Arme', 'Epée', 4000, 38, 2, 2, 'res/img/objets/obj_157.gif');
+	VALUES ('Vitanova', 'Arme', 'Epee', 4000, 38, 2, 2, 'res/img/objets/obj_157.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Jump,Path)
-	VALUES ('Epée mythril', 'Arme', 'Epée', 2000, 33, 1, 'res/img/objets/obj_158.gif');
+	VALUES ('Epee mythril', 'Arme', 'Epee', 2000, 33, 1, 'res/img/objets/obj_158.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Path)
-	VALUES ('Epée victoire', 'Arme', 'Epée', 500, 33, 10, 10, 'res/img/objets/obj_159.gif');
+	VALUES ('Epee victoire', 'Arme', 'Epee', 500, 33, 10, 10, 'res/img/objets/obj_159.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Defense,Resistance,Evasion,Path)
-	VALUES ('Epée bulbe', 'Arme', 'Epée', 500, 29, 5, 5, 10, 'res/img/objets/obj_160.gif');
+	VALUES ('Epee bulbe', 'Arme', 'Epee', 500, 29, 5, 5, 10, 'res/img/objets/obj_160.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack,Path)
-	VALUES ('Chijiraden', 'Arme', 'Epée', 500, 65, 'res/img/objets/obj_161.gif');
+	VALUES ('Chijiraden', 'Arme', 'Epee', 500, 65, 'res/img/objets/obj_161.gif');
 INSERT INTO
 	APP.ITEM (Name,Type,Category,Price,Attack, Magic,Path)
-	VALUES ('Epée de glace', 'Arme', 'Epée', 500, 41, 5, 'res/img/objets/obj_162.gif');
+	VALUES ('Epee de glace', 'Arme', 'Epee', 500, 41, 5, 'res/img/objets/obj_162.gif');
 
